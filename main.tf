636ef3f7a11d7086a9a320edf21d3e52fa4ca514
rike2867 <Rikesh.ManDangol@rackspace.co.uk>
2021-07-28 11:05:41 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+ backend "remote" {
+    organization = "hashicrop-riklearn"
+
+    workspaces {
+      name = "hashicrop-riklearn"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
